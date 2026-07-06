7126d5c3479a28f97841fdbdadbeec1c65dd13de
dhanishkanare83 <dhanishkanare83@gmail.com>
2026-07-06 19:07:21 +0530

    add files via uploaded

---
+++ create_db.py
@@ -0,0 +1,20 @@
+
+import sqlite3
+
+conn = sqlite3.connect("students.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER,
+    course TEXT
+)
+""")
+
+conn.commit()
+conn.close()
+
+print("Database Created Successfully!")
